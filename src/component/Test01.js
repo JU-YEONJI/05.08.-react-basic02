b376f0b1a7f70afdac216d0366109e5c9cad53df
JU-YEONJI <juji1007@naver.com>
2023-05-08 10:25:17 +0900

    2th

---
+++ src/component/Test01.js
@@ -0,0 +1,18 @@
+
+export default function Test01(){
+    return(
+    <>
+    {/* 클래스**네임 */}
+    <div className="intro">
+    <h1>Welcome to my website!</h1>
+    </div>
+    
+    <p className="summary">
+    You can find my thoughts here.
+    {/* br / */}
+    <br /><br />
+    <b>And <i>pictures</i></b> of scientists!
+    </p>
+    </>
+    )
+}
